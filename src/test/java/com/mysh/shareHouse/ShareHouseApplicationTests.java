574f830f6ad17bdaea5d4164907ad1d8af4b0a92
wonmoLee <leewonmo94@gmail.com>
2020-07-28 20:46:14 +0900

    프로젝트 생성과 DB유저생성 및 DB연결 구축

---
+++ src/test/java/com/mysh/shareHouse/ShareHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysh.shareHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
