e9fdd387b76235b5756afb63be8141385f8e565d
wonmoLee <leewonmo94@gmail.com>
2020-08-03 23:47:21 +0900

    팀원들 파일 합치는중

--- src/main/java/com/mysh/shareHouse/controller/indexController.java
+++ src/main/java/com/mysh/shareHouse/controller/indexController.java
@@ -4,15 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class indexController {
+public class IndexController {
 	
 	@GetMapping({"", "/"})
 	public String index() {
 		return "index";
 	}
 	
+	@GetMapping("/moveInGuide")
+	public String moveInGuide() {
+		return "/page/moveInGuide";
+	}
+	
 	@GetMapping("/putOutRoom")
 	public String putOutRoom() {
 		return "/page/putOutRoom";
 	}
+	
+
+	
 }
